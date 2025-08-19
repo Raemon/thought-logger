2d594b01860566fa36a60ab372e1c62b45d47f43
Jake Coble <13357401+jakecoble@users.noreply.github.com>
2025-08-19 12:04:56 -0400

    Add a snapshot test for log rendering

--- tests/renderer/logs.spec.tsx
+++ tests/renderer/logs.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { expect, test, vi } from "vitest";
+import { afterAll, beforeAll, expect, test, vi } from "vitest";
 import { render } from "vitest-browser-react";
 
 import { FileInfo } from "../../src/frontend/FileInfo";
@@ -13,13 +13,22 @@ const userData: UserData = {
   getRecentApps: vi.fn<UserData["getRecentApps"]>().mockResolvedValue(["foo"]),
   getRecentLogs: vi.fn<UserData["getRecentLogs"]>().mockResolvedValue([
     {
-      date: new Date(),
+      date: new Date(2025, 8, 19),
       loading: false,
       scope: SerializedScopeTypes.Day,
       appPath: "fakepath.by-app.log",
       chronoPath: "fakepath.chronological.log",
       rawPath: "fakepath.log",
-      summaryContents: "This is a summary",
+      summaryContents: "This is a daily summary",
+    },
+    {
+      date: new Date(2025, 8, 12),
+      loading: false,
+      scope: SerializedScopeTypes.Week,
+      appPath: "fakepath.by-app.log",
+      chronoPath: "fakepath.chronological.log",
+      rawPath: "fakepath.log",
+      summaryContents: "This is a weekly summary",
     },
   ]),
   getUserDataFolder: vi
@@ -35,7 +44,18 @@ const userData: UserData = {
 
 vi.stubGlobal("userData", userData);
 
+beforeAll(() => {
+  vi.useFakeTimers();
+  const date = new Date(2025, 8, 19);
+  vi.setSystemTime(date);
+});
+
+afterAll(() => {
+  vi.useRealTimers();
+});
+
 test("Renders the logs", async () => {
-  const { getByText } = render(<FileInfo />);
-  await expect.element(getByText("Table of Contents")).toBeInTheDocument();
+  const { asFragment, getByText } = render(<FileInfo />);
+  await expect.element(getByText("This is a daily summary")).toBeVisible();
+  expect(asFragment()).toMatchSnapshot();
 });
