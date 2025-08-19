72ab5099e8df23c42f64ebdf26ca6280e79833fe
Jake Coble <13357401+jakecoble@users.noreply.github.com>
2025-08-19 11:44:04 -0400

    Configure vitest for renderer

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,14 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    include: ["tests/renderer/**/*.spec.{ts,tsx}"],
+    browser: {
+      provider: "playwright",
+      enabled: true,
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
