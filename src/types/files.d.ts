e7a7b84c14458f1d8d1a18fe05f584594b5c5994
Jake Coble <13357401+jakecoble@users.noreply.github.com>
2025-08-20 10:19:19 -0400

    Add new summary types

--- src/types/files.d.ts
+++ src/types/files.d.ts
@@ -13,3 +13,30 @@ export interface SerializedLog {
   scope: SerializedScopeTypes;
   loading: boolean;
 }
+
+export enum SummaryScopeTypes {
+  Day,
+  Week,
+}
+
+export interface Keylog {
+  rawPath: string;
+  appPath: string;
+  chronoPath: string;
+  date: Date;
+}
+
+export interface Screenshot {
+  imagePath: string;
+  summaryPath: string;
+  date: Date;
+}
+
+export interface Summary {
+  date: Date;
+  loading: boolean;
+  keylogs: Keylog[];
+  screenshots: Screenshot[];
+  scope: SummaryScopeTypes;
+  contents: string;
+}
