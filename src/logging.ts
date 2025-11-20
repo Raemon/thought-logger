8c6b7cbe9943d571fd3cac7025cb653c0a048022
Jake Coble <13357401+jakecoble@users.noreply.github.com>
2025-11-20 17:20:59 -0500

    Improve logging format

--- src/logging.ts
+++ src/logging.ts
@@ -1,22 +1,31 @@
 import winston from "winston";
 import { homedir } from "node:os";
 
+const KiB = 1024;
+
 const home = homedir();
 const logPath =
   process.platform === "darwin"
     ? `${home}/Library/Logs/thought-logger/combined.log`
     : `${home}/.local/state/log/thought-logger.log`;
 
+const format = winston.format.combine(
+  winston.format.errors({ stack: true }),
+  winston.format.timestamp(),
+  winston.format.printf(
+    ({ timestamp, level, message, stack }) =>
+      `${timestamp} [${level.toUpperCase()}]: ${stack || message}\n`,
+  ),
+);
+
 const log = winston.createLogger({
+  format,
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
-      ),
-    }),
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: logPath,
+      maxsize: 100 * KiB,
+      maxFiles: 3,
     }),
   ],
 });
